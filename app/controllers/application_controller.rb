d21d8aafd1694e0d05856ae7f2d28598a88dd91c
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2017-06-08 01:25:49 -0300

    style: add devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :application_metadata
+
+  # Method to show some default data in views
+  def application_metadata
+    arr_data = {
+        site_name: "Volunteer",
+        site_description: "Aproximando voluntários de instituições"
+    }
+  end
 end
